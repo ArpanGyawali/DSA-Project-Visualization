05cf0150469c4a9d1f5129c30e82a8d3b1184299
Arpan-Gyawali <46037105+Arpan-Gyawali@users.noreply.github.com>
2020-11-26 11:59:31 +0545

    menu screen added

---
+++ src/menu.cpp
@@ -0,0 +1,138 @@
+#include "../include/menu.hpp"
+
+menu::menu(void)
+{
+	alpha_max = 3 * 255;
+	alpha_div = 3;
+	playing = false;
+	selectedItemIndex = 0;
+}
+
+int menu::getPressedItem()
+{
+	return selectedItemIndex;
+}
+
+int menu::Run(sf::RenderWindow &App)
+{
+	Event event;
+	Texture Texture;
+	Sprite Sprite;
+	bool Running = true;
+	int alpha = 0;
+	Font font;
+	Text Menu[MAX_Number_OF_TEXT];
+	// int menu = 0;
+
+	if (!font.loadFromFile("resource/HeadText.ttf"))
+	{
+		std::cerr << "Error loading HeadText.ttf" << std::endl;
+		return (-1);
+	}
+
+	if (!Texture.loadFromFile("resource/star.jpg"))
+	{
+		std::cerr << "Error loading star.jpg" << std::endl;
+		return (-1);
+	}
+	Sprite.setTexture(Texture);
+	Sprite.setColor(sf::Color(255, 255, 255, alpha));
+
+	for (int i = 0; i < MAX_Number_OF_TEXT; i++) {
+		Menu[i].setFont(font);
+		Menu[i].setPosition(Vector2f((800 / 2)-100+(i*18) , 600 / (MAX_Number_OF_TEXT + 1)*(i+1)));
+		Menu[i].setCharacterSize(30);
+	}
+
+	Menu[0].setFillColor(Color::Red);
+	Menu[0].setString("BUBBLE SORT");
+
+	Menu[1].setFillColor(Color::White);
+	Menu[1].setString("DIJKSTRA");
+
+
+	Menu[2].setFillColor(Color::White);
+	Menu[2].setString("CREDITS");
+
+	Menu[3].setFillColor(Color::White);
+	Menu[3].setString("EXIT");
+
+	if (playing)
+	{
+		alpha = alpha_max;
+	}
+
+	while (Running)
+	{
+		//Verifying events
+		while (App.pollEvent(event))
+		{
+			// Window closed
+			if (event.type == Event::Closed)
+			{
+				return (-1);
+			}
+			//Key pressed
+			if (event.type == Event::KeyPressed)
+			{
+				switch (event.key.code)
+				{
+				case Keyboard::Up:
+					if (selectedItemIndex - 1 >= 0) {
+						Menu[selectedItemIndex].setFillColor(Color::White);
+						selectedItemIndex--;
+						Menu[selectedItemIndex].setFillColor(Color::Red);
+					}
+					break;
+				case Keyboard::Down:
+					if (selectedItemIndex + 1 < MAX_Number_OF_TEXT) {
+						Menu[selectedItemIndex].setFillColor(Color::White);
+						selectedItemIndex++ ;
+						Menu[selectedItemIndex].setFillColor(Color::Red);
+					}
+					break;
+				case Keyboard::Return:
+					switch (getPressedItem())
+                    {
+                    case 0:
+                    	playing = true;
+                        return (1);
+                        break;
+                    case 1:
+                    	playing = true;
+                        return (2);
+                        break;
+                    case 2:
+                    	playing = true;
+                        return (3);
+                        break;
+                    case 3:
+                        return (-1);
+                        break;
+                    default:
+                        break;
+                    }
+				default:
+					break;
+				}
+			}
+		}
+
+		if (alpha<alpha_max)
+		{
+			alpha++;
+		}
+		Sprite.setColor(sf::Color(255, 255, 255, alpha / alpha_div));
+
+		App.clear();
+		App.draw(Sprite);
+
+		if (alpha == alpha_max){
+			for (int i = 0; i < MAX_Number_OF_TEXT; i++) {
+				App.draw(Menu[i]);
+			}
+		}
+		App.display();
+	}
+	return (-1);
+}
