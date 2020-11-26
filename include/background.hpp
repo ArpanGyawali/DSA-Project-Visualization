fb2132967aa95eb145c4cab12d5a39022b3aa640
Arpan-Gyawali <46037105+Arpan-Gyawali@users.noreply.github.com>
2020-11-26 11:46:29 +0545

    for scrolling backgrond
    not implemented yet

---
+++ include/background.hpp
@@ -0,0 +1,16 @@
+// #include <SFML\Graphics.hpp>
+// using namespace sf;
+
+// class Background
+// {
+// public:
+// 	Background(float width,float height);
+// 	void generate(RenderWindow &window);
+// private:
+// 	int posx;
+// 	float frame_time;
+// 	Texture texts;
+// 	Sprite stars;
+// 	Time passed_time;
+// 	Clock clock;
+// };
