8dcc860c21754cbce85af0dae7157fb1c9e3fb30
Arpan-Gyawali <46037105+Arpan-Gyawali@users.noreply.github.com>
2020-11-26 12:06:58 +0545

    for managing all the screens

---
+++ include/screens.hpp
@@ -0,0 +1,13 @@
+#ifndef SCREENS_HPP_INCLUDED
+#define SCREENS_HPP_INCLUDED
+
+//Basic Screen Class
+#include "cScreen.hpp"
+
+//Including each screen of application
+#include "menu.hpp"
+#include "credits.hpp"
+
+
+
+#endif // SCREENS_HPP_INCLUDED
