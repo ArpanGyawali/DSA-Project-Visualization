a4008d280cedab64c9918dea6ea3953f845944a5
Arpan-Gyawali <46037105+Arpan-Gyawali@users.noreply.github.com>
2020-11-26 11:56:55 +0545

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "../include/screens.hpp"
+//#include "../include/background.hpp"
+
+
+int main()
+{
+    //Applications variables
+    std::vector<cScreen*> Screens;
+    int screen = 0;
+
+    //Window creation
+    sf::RenderWindow App(sf::VideoMode(800, 600), "DSA PROJECT");
+    //Background background(800, 600);
+
+    //Screens preparations
+    menu s0;
+    Screens.push_back(&s0);
+    Credits s1;
+    Screens.push_back(&s1);
+    Djks s2;
+    Screens.push_back(&s2);
+
+    //Main loop
+    while (screen >= 0)
+    {
+        //background.generate(App);
+        screen = Screens[screen]->Run(App);
+    }
+
+    return 0;
+}
