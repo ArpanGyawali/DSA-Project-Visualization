49590175ff5b50941cb7b106786a2b50a1f3e916
Arpan-Gyawali <arpan.gyawali7@gmail.com>
2020-12-05 08:51:41 +0545

    Bubble Sort and background added

--- include/Quick.hpp
+++ include/Quick.hpp
@@ -10,8 +10,8 @@
 #include <ctime>
 #include "cScreen.hpp"
 
-#define ARR_MAX_VALUE 150
-#define CHUNK_ARRAY_SIZE 15
+#define ARR_MAX_VALUE 100
+#define CHUNK_ARRAY_SIZE 10
 
 using namespace std;
 using namespace sf;
@@ -20,25 +20,32 @@ class QuickSort : public cScreen
 {
 private:
 	std::array<int, ARR_MAX_VALUE> arr;
+	std::array<int, ARR_MAX_VALUE> arr1;
 	std::array<int, ARR_MAX_VALUE> partitionIndexes;
+	std::array<int, ARR_MAX_VALUE> partitionIndexes1;
 	RectangleShape buttonRandom;
 	RectangleShape buttonDescending;
 	RectangleShape buttonAlmostSorted;
 	RectangleShape buttonSort;
 	RectangleShape rectangle;
+	RectangleShape rectangle1;
 	RectangleShape rectPointer;
 	Font font;
+	Font font1;
 	Text text1;
 	Text text2;
 	Text text3;
 	Text text;
+	Text text4;
 
 public:
 	QuickSort(void);
 	void sortArray(std::array<int, ARR_MAX_VALUE>* arr, int low, int high);
+	void bubbleSort(std::array<int, ARR_MAX_VALUE>* arr1);
 	void fillArrayRandom();
 	void fillArrayDescending();
 	void fillArrayAlmostSorted();
+	void reset();
 	int partition(std::array<int, ARR_MAX_VALUE>* arr, int low, int high);
 	void swap(int* a, int* b);
 	virtual int Run(RenderWindow &App);
--- src/Quick.cpp
+++ src/Quick.cpp
@@ -1,46 +1,79 @@
 #include "../include/Quick.hpp"
 
 QuickSort::QuickSort(void) {
-	for (size_t i = 0; i < arr.size(); i++) {
-		arr[i] = (i+1) * (600/ arr.size());
-	}
-	//arr = {0};
-	partitionIndexes = {0};
+	reset();
+	buttonRandom.setSize(Vector2f(196,35));   //button generate random fill
+    buttonRandom.setFillColor(Color::Transparent);
+    buttonRandom.setOutlineThickness(1);
+    buttonRandom.setOutlineColor(Color::Red);
+    buttonDescending.setSize(Vector2f(196,35));   //button generate random fill
+    buttonDescending.setFillColor(Color::Transparent);
+    buttonDescending.setOutlineThickness(1);
+    buttonDescending.setOutlineColor(Color::Red);
+    buttonAlmostSorted.setSize(Vector2f(196,35));   //button generate random fill
+    buttonAlmostSorted.setFillColor(Color::Transparent);
+    buttonAlmostSorted.setOutlineThickness(1);
+    buttonAlmostSorted.setOutlineColor(Color::Red);
+    buttonSort.setSize(Vector2f(196,35));			//button to launch sorting
+    buttonSort.setFillColor(Color::Transparent);
+    buttonSort.setOutlineThickness(1);
+    buttonSort.setOutlineColor(Color::Red);
 
-	buttonRandom.setSize(Vector2f(100,30));   //button generate random fill
-    buttonRandom.setFillColor(Color::Green);
-    buttonDescending.setSize(Vector2f(100,30));   //button generate random fill
-    buttonDescending.setFillColor(Color(126,0,0));
-    buttonAlmostSorted.setSize(Vector2f(100,30));   //button generate random fill
-    buttonAlmostSorted.setFillColor(Color::Blue);
-    buttonSort.setSize(Vector2f(100,30));			//button to launch sorting
-    buttonSort.setFillColor(Color(148,0,211));
-
-	rectangle.setFillColor(sf::Color::White);		//rectangular shae for arrays
+	rectangle.setFillColor(sf::Color::White);		//rectangular shape for array arr
 	rectangle.setOutlineThickness(1.f);
 	rectangle.setOutlineColor(sf::Color(0, 0, 0));
 
+	rectangle1.setFillColor(sf::Color::White);		//rectangular shape for array arr1
+	rectangle1.setOutlineThickness(1.f);
+	rectangle1.setOutlineColor(sf::Color(0, 0, 0));
+
 	rectPointer.setFillColor(sf::Color::Red);		//rectangular shape for pointer
 
-	if (!font.loadFromFile("resource/Text.ttf"))
+	if (!font.loadFromFile("resource/Mono.ttf"))
 	{
 		std::cerr << "Error loading Text.ttf" << std::endl;
 	}
-	text1.setFont(font);
+	if (!font1.loadFromFile("resource/Texturina.ttf"))
+	{
+		std::cerr << "Error loading Text.ttf" << std::endl;
+	}
+	text1.setFont(font1);
 	text1.setString("RANDOM");
-	text1.setCharacterSize(15);
+	text1.setCharacterSize(25);
+	text1.setFillColor(Color::Red);
 
-	text2.setFont(font);
+	text2.setFont(font1);
 	text2.setString("DESCENDING");
-	text2.setCharacterSize(12);
+	text2.setCharacterSize(25);
+	text2.setFillColor(Color::Red);
 
-	text3.setFont(font);
-	text3.setString("ALMOST \nSORTED");
-	text3.setCharacterSize(10);
+	text3.setFont(font1);
+	text3.setString("ALMOST SORTED");
+	text3.setCharacterSize(21);
+	text3.setFillColor(Color::Red);
+
+	text4.setFont(font1);
+	text4.setString("SORT");
+	text4.setCharacterSize(25);
+	text4.setFillColor(Color::Red);
 
 	text.setFont(font);
-	text.setString("SORT");
-	text.setCharacterSize(15);
+    text.setString("SORTING");
+    text.setCharacterSize(45);
+    text.setFillColor(Color::Red);
+}
+
+void QuickSort::reset() {
+	for (size_t i = 0; i < arr.size(); i++) {
+		arr[i] = (i+1) * (600/ arr.size());
+	}
+
+	for (size_t j = 0; j < arr1.size(); j++) {
+		arr1[j] = (j+1) * (600/ arr1.size());
+	}
+	//arr = {0};
+	partitionIndexes = {0};
+	partitionIndexes1 = {0};
 }
 
 void QuickSort::fillArrayRandom() {
@@ -117,6 +150,27 @@ int QuickSort::partition(std::array<int, ARR_MAX_VALUE>* ptArr, int low, int hig
 	return (i + 1);
 }
 
+void QuickSort::bubbleSort(std::array<int, ARR_MAX_VALUE>* pArr)
+{
+	//int n = ARR_MAX_VALUE;
+
+	std::array<int, ARR_MAX_VALUE> arr1 = *pArr;
+	for (int i = 0; i < ARR_MAX_VALUE - 1; i++){			//for no of passes
+		int count = 0;
+		for (int j = 0; j < ARR_MAX_VALUE - i - 1; j++){		//for iteraion (ignoring the last sorted element)
+			partitionIndexes1[j] = 1;
+			if (arr1[j] > arr1[j + 1]){
+				swap(&(*pArr)[j], &(*pArr)[j+1]);
+				//std::swap(arr1[j],arr1[j+1]);
+				count ++;
+			}
+			partitionIndexes1[j] = 0;
+		}
+		if (count == 0)
+			break;
+	}
+}
+
 void QuickSort::swap(int* a, int* b) {
 	int t = *a;
 	*a = *b;
@@ -126,6 +180,14 @@ void QuickSort::swap(int* a, int* b) {
 
 int QuickSort::Run(RenderWindow &App) {
 	Thread threadQ(std::bind(&QuickSort::sortArray,this,&arr,0,int(arr.size()-1)));
+	Thread threadB(std::bind(&QuickSort::bubbleSort,this,&arr1));
+	Texture Texture;
+    Sprite Sprite;
+    if (!Texture.loadFromFile("resource/red1.png"))
+    {
+        std::cerr << "Error loading one.jpg" << std::endl;
+    }
+    Sprite.setTexture(Texture);
 	Event event;
 	bool Running = true;
 	while (Running)
@@ -141,9 +203,14 @@ int QuickSort::Run(RenderWindow &App) {
 				switch (event.key.code)
 				{
 				case sf::Keyboard::Escape:
+					threadB.terminate();
+					threadQ.terminate();
+					reset();
 					return (0);
 					break;
 				case sf::Keyboard::Q:
+					threadB.terminate();
+					threadQ.terminate();
 					return (-1);
 					break;
 				default:
@@ -154,46 +221,68 @@ int QuickSort::Run(RenderWindow &App) {
 			{
                 int X=event.mouseButton.x;
                 int Y=event.mouseButton.y;
-                if(X>700 && X<800 && Y>0 && Y<30){
+                if(X>302 && X<498 && Y>70 && Y<105){
+                	reset();
+                	threadB.terminate();
+					threadQ.terminate();
                 	fillArrayRandom();
+                	for(int i=0; i<ARR_MAX_VALUE; ++i)
+        				arr1[i] = arr[i];
                 	//auto f = std::async(&QuickSort::sortArray, this ,arr, 0, arr.size() - 1);
 					//sortArray(&arr, 0, int(arr.size() - 1));
 				}
-                else if(X>700 && X<800 && Y>40 && Y<70){
+                else if(X>302 && X<498 && Y>105 && Y<140){
+                	reset();
+                	threadB.terminate();
+					threadQ.terminate();
                 	fillArrayDescending();
+                	for(int i=0; i<ARR_MAX_VALUE; ++i)
+        				arr1[i] = arr[i];
                 	//auto f = std::async(&QuickSort::sortArray, this ,arr, 0, arr.size() - 1);
 				}
-                else if(X>700 && X<800 && Y>80 && Y<110){
+                else if(X>302 && X<498 && Y>140 && Y<175){
+                	reset();
+                	threadB.terminate();
+					threadQ.terminate();
                 	fillArrayAlmostSorted();
+                	for(int i=0; i<ARR_MAX_VALUE; ++i)
+        				arr1[i] = arr[i];
                 	//auto f = std::async(&QuickSort::sortArray, this ,arr, 0, arr.size() - 1);
 					//sortArray(&arr, 0, int(arr.size() - 1));
 				}
-				else if(X>700 && X <800 && Y>120 && Y<150){
+				else if(X>302 && X<498 && Y>175 && Y<210){
+					threadB.launch();
 					threadQ.launch();
 				}
+				// else if(X>700 && X <800 && Y>160 && Y<190){
+				// 	threadB.launch();
+				// }
             }
 		}
 
 		App.clear(Color::Black);
+		App.draw(Sprite);
 
-		buttonRandom.setPosition(700,0);
-		buttonDescending.setPosition(700,40);
-		buttonAlmostSorted.setPosition(700,80);
-		buttonSort.setPosition(700,120);
+		buttonRandom.setPosition(302,70);
+		buttonDescending.setPosition(302,105);
+		buttonAlmostSorted.setPosition(302,140);
+		buttonSort.setPosition(302,175);
 		App.draw(buttonRandom);
 		App.draw(buttonDescending);
 		App.draw(buttonAlmostSorted);
 		App.draw(buttonSort);
-		text.setPosition(705,125);
-		text1.setPosition(702,5);
-		text2.setPosition(702,45);
-		text3.setPosition(705,82);
+		text.setPosition(310,20);
+		text1.setPosition(330,73);
+		text2.setPosition(310,108);
+		text3.setPosition(304,143);
+		text4.setPosition(355,178);
 		App.draw(text);
 		App.draw(text1);
 		App.draw(text2);
 		App.draw(text3);
+		App.draw(text4);
 
-		float recXsize = ((App.getSize().x)-100)/ arr.size();
+		float recXsize = ((App.getSize().x)-500)/ arr.size();
 	
 		for(size_t i = 0 ; i < arr.size() ; i++){
 			rectangle.setSize(sf::Vector2f(recXsize, arr[i]));
@@ -201,10 +290,24 @@ int QuickSort::Run(RenderWindow &App) {
 			App.draw(rectangle);
 		}
 
+		for(size_t i = 0 ; i < arr1.size() ; i++){
+			rectangle1.setSize(sf::Vector2f(recXsize, arr1[i]));
+			rectangle1.setPosition(recXsize*i + 500, (App.getSize().y - arr1[i]));
+			App.draw(rectangle1);
+		}
+
 		for (size_t i = 0; i < partitionIndexes.size(); i++){
 			if(partitionIndexes[i] == 1){
 				rectPointer.setSize(sf::Vector2f(recXsize, App.getSize().y));
 				rectPointer.setPosition(recXsize*i + recXsize, 0);
+				App.draw(rectPointer);
+			}
+		}
+
+		for (size_t i = 0; i < partitionIndexes1.size(); i++){
+			if(partitionIndexes1[i] == 1){
+				rectPointer.setSize(sf::Vector2f(recXsize, App.getSize().y));
+				rectPointer.setPosition(recXsize*i + 500 + recXsize, 0);
 				App.draw(rectPointer);
 			}
 		}
