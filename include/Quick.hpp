c7a69e5bc2e55b74425e43312c6f387335c60ff0
Arpan-Gyawali <arpan.gyawali7@gmail.com>
2020-12-10 10:03:36 +0545

    Shell and Insertion Sort added

--- include/Quick.hpp
+++ include/Quick.hpp
@@ -21,17 +21,24 @@ using namespace sf;
 class SortBattle : public cScreen
 {
 private:
+	bool select1,select2,select3,select4;
 	std::array<int, ARR_MAX_VALUE> arr;
 	std::array<int, ARR_MAX_VALUE> arr1;
 	std::array<int, ARR_MAX_VALUE> partitionIndexes;
 	std::array<int, ARR_MAX_VALUE> partitionIndexes1;
+	std::array<int, ARR_MAX_VALUE> arr2;
+	std::array<int, ARR_MAX_VALUE> arr3;
+	std::array<int, ARR_MAX_VALUE> partitionIndexes2;
+	std::array<int, ARR_MAX_VALUE> partitionIndexes3;
 	RectangleShape buttonRandom;
 	RectangleShape buttonDescending;
 	RectangleShape buttonAlmostSorted;
 	RectangleShape buttonSort;
+	RectangleShape Bubble,Quick,Shell,Insertion;
 	RectangleShape rectangle;
 	RectangleShape rectangle1;
 	RectangleShape rectPointer;
+	Time t1,t2,t3,t4;
 	Font font;
 	Font font1;
 	Font font2;
@@ -44,16 +51,21 @@ private:
 	Text text6;
 	Text text7;
 	Text text8;
+	Text text9,text10,text11,text12;
 
 public:
 	SortBattle(void);
 	void QuickSort(std::array<int, ARR_MAX_VALUE>* arr, int low, int high);
+	int partition(std::array<int, ARR_MAX_VALUE>* arr, int low, int high);
 	void bubbleSort(std::array<int, ARR_MAX_VALUE> *arr1);
+	void ShellSort(std::array<int, ARR_MAX_VALUE> *arr2);
+	void InsertionSort(std::array<int, ARR_MAX_VALUE> *arr3);
+	void draw1(std::array<int, ARR_MAX_VALUE> &arr, std::array<int, ARR_MAX_VALUE> &partitionIndexes, RenderWindow& App);
+	void draw2(std::array<int, ARR_MAX_VALUE> &arr1, std::array<int, ARR_MAX_VALUE> &partitionIndexes1, RenderWindow& App);
 	void fillArrayRandom();
 	void fillArrayDescending();
 	void fillArrayAlmostSorted();
 	void reset();
-	int partition(std::array<int, ARR_MAX_VALUE>* arr, int low, int high);
 	void swap(int* a, int* b);
 	virtual int Run(RenderWindow &App);
 };
